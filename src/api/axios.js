4003c09a4b23952edf94005c706c7eecc4661430
stevchen99 <stevchen99@gmail.com>
2026-09-02 18:28:40 +0200

    Netlify problem

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_REACT_APP_API_URL || 'http://localhost:5000/api';
+
 const api = axios.create({
- // baseURL: 'https://fantastic-bassoon-q4p6w6665g934vrr-5000.app.github.dev/api', 
- baseURL: 'http://localhost:5000/api', 
+	baseURL: API_URL,
 });
 
 export default api;
